d7b7dd67f3bcf53524de66e476aecaca89329cfe
Ross Benzie <rossbenzie@gmail.com>
2020-07-16 09:51:50 +0100

    Create add notes functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,34 @@
+import { useState } from 'react'
+import produce from 'immer'
+
 const Notes = props => props.notes.map(note => <div>{note.text}</div>)
 
 export default () => {
-
-
-  const notes = [
+  const initialNotes = [
     { text: "Do something" }, { text: "Do something else" }
   ]
+  const [notes, setNotes] = useState(initialNotes)
 
-  return <Notes notes={notes} />
+  const handleClick = () => {
+    const text = document.getElementById("noteInput").value.trim()
+    if (text) {
+      const nextNotes = produce(notes, interimNotes => {
+        interimNotes.push({ text })
+      })
+      document.getElementById('noteInput').value = ''
+      setNotes(nextNotes)
+    }
+  }
 
+  return (
+    <>
+      <div>
+
+        <input id="noteInput" type="text" placeholder="Add a note" />
+        <button onClick={() => handleClick()}>Add Note</button>
+
+        <Notes notes={notes} />
+      </div>
+    </>
+  )
 }
